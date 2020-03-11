e33ca2d2817cd5f0e6e811e98bca2721923e8258
austin <austinanthony226@yahoo.com>
2020-03-10 23:54:50 -0400

    parstaGram

---
+++ parstaGram/cameraViewController.swift
@@ -0,0 +1,48 @@
+//
+//  cameraViewController.swift
+//  parstaGram
+//
+//  Created by Austin Anthony on 3/10/20.
+//  Copyright © 2020 Austin Anthony. All rights reserved.
+//
+
+import UIKit
+
+class cameraViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var commentField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    @IBAction func onSubmit(_ sender: Any) {
+    }
+    
+    @IBAction func onCameraButton(_ sender: Any) {
+        let picker = UIImagePickerController()
+        picker.delegate = self
+        picker.allowsEditing = true
+        
+        if UIImagePickerController.isSourceTypeAvailable(.camera) {
+            picker.sourceType = .camera
+        } else {
+            picker.sourceType = .photoLibrary
+        }
+        
+        present(picker, animated: true, completion: nil)
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
